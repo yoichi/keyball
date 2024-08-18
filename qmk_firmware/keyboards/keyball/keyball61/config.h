0c3e9e482a12bd941c32ede2bb71665ebed86c20
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2024-08-18 21:07:25 +0900

    change default auto mouse timeout to 450ms

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -76,3 +76,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+#define AUTO_MOUSE_TIME 450
