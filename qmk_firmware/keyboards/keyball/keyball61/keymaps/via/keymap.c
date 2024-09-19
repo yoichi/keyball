2c7a2adb9a083c028b8f87b52ea84dd80d525040
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2024-09-19 21:22:56 +0900

    keymap 20240919
    * bind G(KC_SPC)/A(KC_GRV) for IME toggle on Mac/Win
    * unbind KC_LNG1 and KC_LNG2
    * bind symbols in layer 1

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -25,18 +25,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
+    G(KC_SPC), KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , LT(3,KC_BSLS),
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , RCTL_T(KC_EQL),
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,LT(2,KC_QUOT),         KC_BTN1  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT,
-    MO(1),LCTL_T(KC_GRV), KC_LALT  , KC_LGUI  , KC_LNG2  , KC_SPC   ,LT(3,KC_LNG1),         KC_BSPC,LT(2,KC_ENT), _______  , _______  , _______  ,RALT_T(KC_LBRC),LT(1,KC_RBRC)
+LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , RCTL_T(KC_EQL),
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,LT(2,KC_QUOT),         KC_GRV   , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT,
+    MO(1)    , KC_LCTL  , KC_LALT  , KC_LGUI  , KC_LCTL  , KC_SPC   , MO(3)    ,            KC_BSPC,LT(2,KC_ENT), _______  , _______  , _______  , KC_RALT  , MO(1)
   ),
 
   [1] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_F12   ,
-    _______  , KC_PSCR  , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
-    _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_INT1  ,            KC_CAPS  , KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______  ,
+    _______  , KC_PSCR  , KC_GRV   , KC_QUOT  , KC_LBRC  , KC_RBRC  ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
+    _______  ,A(KC_PSCR), S(KC_GRV),S(KC_QUOT),S(KC_LBRC),S(KC_RBRC), KC_INT1  ,            KC_CAPS  , KC_MINS  ,S(KC_MINS), KC_EQL   , S(KC_EQL),S(KC_BSLS), _______  ,
     _______  , _______  , _______  , _______  , KC_HOME  , KC_DEL   , KC_INT3  ,            _______  , KC_END   , _______  , _______  , _______  , _______  , _______
   ),
 
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BRMU  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  ,KC_KB_MUTE,KC_KB_VOLUME_UP,
     KC_BRMD  , _______  , _______  , _______  , _______  , _______  ,                                  KC_BTN4  , KC_WH_R  , KC_WH_D  , KC_WH_L  , _______  ,KC_KB_VOLUME_DOWN,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  KC_PGUP  , _______  , KC_WH_U  , _______  , KC_BTN3  , _______  ,
-    KC_BTN1  , _______  , _______  , _______  , _______  , _______  , _______  ,            KC_BTN1  , KC_PGDN  , _______  , _______  , _______  , _______  , KC_BTN2  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            A(KC_GRV), KC_PGDN  , _______  , _______  , _______  , _______  , KC_BTN2  ,
     _______  , KC_BTN3  , KC_BTN2  , _______  , KC_BTN1  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , KC_BTN5  , _______
   ),
 
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , AML_TO   , AML_I50  , AML_D50  , _______  , EE_CLR   ,                                  SSNP_FRE , SSNP_VRT , SSNP_HOR , SREV_VRT , SREV_HOR , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  KC_BTN4  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , _______  ,
     KBC_RST  , KBC_SAVE , CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  ,                                  KC_PGUP  , _______  , KC_DOWN  , _______  , KC_BTN3  , _______  ,
-    KC_BTN1  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , _______  ,            KC_BTN1  , KC_PGDN  , AG_LSWP  , AG_LNRM  , _______  , _______  , KC_BTN2  ,
+    _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , _______  ,            _______  , KC_PGDN  , AG_LSWP  , AG_LNRM  , _______  , _______  , KC_BTN2  ,
     _______  , KC_BTN3  , KC_BTN2  , _______  , KC_BTN1  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , KC_BTN5  , _______
   ),
 };
