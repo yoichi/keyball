1e9f9b6ec83fdfdf546e2613f80b82bdb746b746
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2024-11-17 00:38:00 +0900

    keymap 20241117
    * move KC_GRV and KC_QUOT to right hand side
    * bind RGUI(KC_1) - RGUI(KC_4) for app shortcut on Windows
    * bind G(KC_1) - G(KC_9) in layer3 for tab selection on macOS
    * move G(KC_SPC) from TAB position in layer1 to SPC position in layer3

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -32,21 +32,21 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D    , KC_F     , KC_G     ,            
   ),
 
   [1] = LAYOUT_universal(
-    G(KC_SPC),  KC_1    , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , _______  ,
-    _______  ,  KC_GRV  , KC_QUOT  , KC_LBRC , KC_RBRC  , KC_BSLS  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
-    _______  ,  _______ , _______  , _______ , _______  , KC_DEL   ,                                         KC_INT1  ,S(KC_INT1), KC_INT3  ,S(KC_INT3), KC_CAPS  , _______  ,
+    _______  ,  KC_1    , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , _______  ,
+    _______  ,  _______ , _______  , KC_LBRC , KC_RBRC  , KC_BSLS  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
+    _______  ,RGUI(KC_1),RGUI(KC_2),RGUI(KC_3),RGUI(KC_4),KC_DEL   ,                                         KC_GRV   , KC_QUOT  , KC_INT1  , KC_INT3  , KC_CAPS  , _______  ,
                   _______  , _______ , KC_HOME  ,         _______  , _______  ,                   _______  , KC_END   , _______       , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
     _______  ,  S(KC_1) , S(KC_2)  , S(KC_3) , S(KC_4)  , S(KC_5)  ,                                         S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , _______  ,
-    _______  ,S(KC_GRV),S(KC_QUOT),S(KC_LBRC),S(KC_RBRC),S(KC_BSLS),                                         _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-                  _______  , KC_BTN2 , KC_BTN1  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______  ,  _______ , _______ ,S(KC_LBRC),S(KC_RBRC),S(KC_BSLS),                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+    _______  ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         S(KC_GRV),S(KC_QUOT),S(KC_INT1),S(KC_INT3), _______  , _______  ,
+                  _______  , KC_BTN2 , KC_BTN1  ,        G(KC_SPC) , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
-    MO(4)    , _______  , _______  , _______ , _______  , _______  ,                                         _______  , _______  , _______  , _______  , _______  , _______  ,
+    MO(4)    , G(KC_1)  , G(KC_2)  , G(KC_3) , G(KC_4)  , G(KC_5)  ,                                         G(KC_6)  , G(KC_7)  , G(KC_8)  , G(KC_9)  , _______  , _______  ,
     _______  , _______  , _______  , _______ , _______  , _______  ,                                         KC_PGUP  , _______  , _______  ,KC_BRMU,KC_KB_VOLUME_UP, _______,
     _______  , _______  , _______  , _______ , _______  , _______  ,                                         KC_PGDN  , _______  , _______  ,KC_BRMD,KC_KB_VOLUME_DOWN,_______,
                   _______  , _______ , _______  ,         KC_WH_L  , _______  ,                RALT(KC_GRV), KC_WH_R  , _______       , _______  , KC_KB_MUTE
