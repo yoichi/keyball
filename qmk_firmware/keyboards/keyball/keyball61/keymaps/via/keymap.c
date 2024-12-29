84c741beacdb78c08d7aee32987eb526c801bd33
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2024-12-29 09:14:43 +0900

    Move KC_HOME, KC_END in layer 1 to make KC_ENT transparent

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -41,8 +41,8 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,           
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_F12   ,
     _______  , KC_INT1  , KC_INT3  , KC_GRV   , KC_LBRC  , KC_RBRC  ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
-    _______  , S(KC_HOME),S(KC_END), _______  , _______  , _______  , KC_DEL   ,            KC_CAPS  , KC_QUOT  , KC_BSLS  , _______  , _______  , _______  , _______  ,
-    _LAYER_  , _______  , _______  , _______  , KC_HOME  , _LAYER_  , _______  ,            _______  , KC_END   , _______  , _______  , _______  , _______  , _LAYER_
+    _______  , S(KC_HOME),S(KC_END), KC_HOME  , KC_END   , _______  , KC_DEL   ,            KC_CAPS  , KC_QUOT  , KC_BSLS  , _______  , _______  , _______  , _______  ,
+    _LAYER_  , _______  , _______  , _______  , _______  , _LAYER_  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _LAYER_
   ),
 
   [2] = LAYOUT_universal(
