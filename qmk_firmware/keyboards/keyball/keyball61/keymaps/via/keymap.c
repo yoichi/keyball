f055b48db22d9ee1de7d47c1b2707765684fdb16
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2025-01-29 05:58:27 +0900

    Move S(KC_HOME), S(KC_END) to center

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -41,7 +41,7 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,           
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_F12   ,
     _______  , KC_INT1  , KC_INT3  , KC_GRV   , KC_LBRC  , KC_RBRC  ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
-    _______  , S(KC_HOME),S(KC_END), KC_HOME  , KC_END   , _______  , KC_DEL   ,            KC_CAPS  , KC_QUOT  , KC_BSLS  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  ,S(KC_HOME), S(KC_END), _______  , KC_DEL   ,            KC_CAPS  , KC_QUOT  , KC_BSLS  , _______  , _______  , _______  , _______  ,
     _LAYER_  , _______  , _______  , _______  , KC_HOME  , _LAYER_  , KC_END   ,            _______  , _______  , _______  , _______  , _______  , _______  , _LAYER_
   ),
 
@@ -50,7 +50,7 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,           
     _LAYER_  , S(KC_1)  , S(KC_2)  , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , _LAYER_  ,
     _______  ,S(KC_INT1),S(KC_INT3), S(KC_GRV),S(KC_LBRC),S(KC_RBRC),                        RWIN(KC_LEFT),RWIN(KC_DOWN),RWIN(KC_UP),RWIN(KC_RGHT),_______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _LAYER_  ,            _______  ,S(KC_QUOT),S(KC_BSLS), _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , KC_BTN2  , KC_BTN1  , _______  , _______  ,            _______  , _LAYER_  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , KC_BTN2  , KC_BTN1  ,S(KC_HOME), S(KC_END),            _______  , _LAYER_  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
