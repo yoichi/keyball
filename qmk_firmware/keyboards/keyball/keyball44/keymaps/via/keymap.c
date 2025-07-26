3818ece6552d687f4a5cf0bed4ed864fdd13b7ca
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2025-07-26 15:03:37 +0900

    assign RCTL(KC_RALT) in layer1

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -41,7 +41,7 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D    , KC_F     , KC_G     ,            
     KC_QUOT  , KC_1     , KC_2     , KC_3    , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSLS  ,
     _______  , KC_INT1  , KC_INT3  , KC_GRV  , KC_LBRC  , KC_RBRC  ,                                         KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
     _______  , KC_CAPS  , _______  ,S(KC_HOME),S(KC_END), KC_DEL   ,                                         _______  , _______  , _______  , _______  , _______  , _______  ,
-                  _______  , _______ , KC_HOME  ,         _LAYER_  , KC_END   ,                   _______  , _______  , _______       , _______  , _______
+                  _______  , _______ , KC_HOME  ,         _LAYER_  , KC_END   ,                   _______  , _______  , _______       , _______  , RCTL(KC_RALT)
   ),
 
   [2] = LAYOUT_universal(
