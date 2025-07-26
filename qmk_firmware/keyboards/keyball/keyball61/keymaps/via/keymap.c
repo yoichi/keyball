3ac6e778b1c5bb60c31a66d07ce1aa172cef2817
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2025-07-26 22:47:16 +0900

    LT(1,) -> LT(2,) on KC_EQL in layer0, assign RCTL(KC_RALT) in layer1

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -31,7 +31,7 @@ enum custom_user_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSLS  ,
- LT(2,KC_TAB), KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , LT(2,KC_EQL),
+ LT(2,KC_TAB), KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , LT(1,KC_EQL),
 LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , RCTL_T(KC_MINS),
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,LT(2,KC_GRV),          KC_QUOT  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
     MO(1)    , KC_LCTL  , KC_LALT  , KC_LGUI  , KC_BTN1,LT(1,KC_SPC), MO(3)    ,            KC_BSPC,LT(2,KC_ENT), _______  , _______  , _______  , KC_RALT  , MO(1)
@@ -42,7 +42,7 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,           
     KC_QUOT  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSLS  ,
     _______  , KC_INT1  , KC_INT3  , KC_GRV   , KC_LBRC  , KC_RBRC  ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_INS   , _______  ,
     _______  , _______  , _______  ,S(KC_HOME), S(KC_END), _______  , KC_DEL   ,            KC_CAPS  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _LAYER_  , _______  , _______  , _______  , KC_HOME  , _LAYER_  , KC_END   ,            _______  , _______  , _______  , _______  , _______  , _______  , _LAYER_
+    _LAYER_  , _______  , _______  , _______  , KC_HOME  , _LAYER_  , KC_END   ,            _______  , _______  , _______  , _______  ,_______,RCTL(KC_RALT), _LAYER_
   ),
 
   [2] = LAYOUT_universal(
