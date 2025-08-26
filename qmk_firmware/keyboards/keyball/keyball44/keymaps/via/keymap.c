260f0ea305a415f43d8b11880470bed0af258e93
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2025-08-26 18:09:53 +0900

    virtual desktop selection and arrange window on Windows and macOS
    with Rectangle (https://rectangleapp.com/) on macOS

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -61,7 +61,7 @@ LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D    , KC_F     , KC_G     ,            
   [_MEDIA] = LAYOUT_universal(
   MO(_CONFIG), G(KC_1)  , G(KC_2)  , G(KC_3) , G(KC_4)  , G(KC_5)  ,                                         G(KC_6)  , G(KC_7)  , G(KC_8)  , G(KC_9)  , KC_BRIU  , KC_VOLU  ,
     _______  , _______  , _______  , _______ , _______  , KC_PGUP  ,                                         C(KC_LEFT),C(KC_DOWN),C(KC_UP) ,C(KC_RGHT), KC_BRID  , KC_VOLD  ,
-    KC_LSFT  ,RWIN(KC_1),RWIN(KC_2),RWIN(KC_3),RWIN(KC_4),KC_PGDN  ,                                 RWIN(KC_LEFT),RWIN(KC_DOWN),RWIN(KC_UP),RWIN(KC_RGHT),_______, KC_MUTE  ,
+    KC_LSFT  ,RWIN(KC_1),RWIN(KC_2),RWIN(KC_3),RWIN(KC_4),KC_PGDN  ,                                     LCA(KC_LEFT),LCA(KC_DOWN),LCA(KC_UP),LCA(KC_RGHT),_______, KC_MUTE  ,
                   _______  , _______ , _______  ,         _______  , _LAYER_  ,                   IME_TGL  , _______  , XXXXXXX       , XXXXXXX  , _______
   ),
 
@@ -282,6 +282,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     register_code16(KC_LALT);
                 } else {
                     unregister_code16(KC_LALT);
+                }
+                return false;
+            default:
+                break;
+            }
+            break;
+        case LCA(KC_LEFT):
+        case LCA(KC_DOWN):
+        case LCA(KC_UP):
+        case LCA(KC_RGHT):
+            switch (detected_host_os()) {
+            case OS_WINDOWS:
+                keycode ^= (QK_LCTL | QK_LALT);
+                keycode = RWIN(keycode);
+                if (record->event.pressed) {
+                    register_code16(keycode);
+                } else {
+                    unregister_code16(keycode);
+                }
+                return false;
+            default:
+                break;
+            }
+            break;
+        case C(KC_LEFT):
+        case C(KC_DOWN):
+        case C(KC_UP):
+        case C(KC_RGHT):
+            switch (detected_host_os()) {
+            case OS_WINDOWS:
+                keycode = RWIN(keycode);
+                if (record->event.pressed) {
+                    register_code16(keycode);
+                } else {
+                    unregister_code16(keycode);
                 }
                 return false;
             default:
