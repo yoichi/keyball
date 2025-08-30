74289ccda2002a9a65b9f6c0d5fa912c8858719d
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2025-08-30 16:20:44 +0900

    swap KC_QUOT and KC_BSLS in default layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 LT(_SYMBOL,KC_TAB), KC_Q, KC_W     , KC_E    , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , LT(_NUMBER,KC_EQL),
 LCTL_T(KC_ESC),KC_A     , KC_S     , KC_D    , KC_F     , KC_G     ,                                         KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , RCTL_T(KC_MINS),
     KC_LSFT  , KC_Z     , KC_X     , KC_C    , KC_V     , KC_B     ,                                         KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-                  KC_LALT  , KC_LGUI , KC_BTN1,LT(_NUMBER,KC_SPC)  ,LT(_MEDIA,KC_BSLS),     KC_BSPC,LT(_SYMBOL,KC_ENT), XXXXXXX       , XXXXXXX  , RALT_T(KC_QUOT)
+                  KC_LALT  , KC_LGUI , KC_BTN1,LT(_NUMBER,KC_SPC)  ,LT(_MEDIA,KC_QUOT),     KC_BSPC,LT(_SYMBOL,KC_ENT), XXXXXXX       , XXXXXXX  , RALT_T(KC_BSLS)
   ),
 
   [_NUMBER] = LAYOUT_universal(
